3375db4f4d96be26061ae08b0d0822754b770327
Sevga <sevgama44@gmail.com>
2020-08-22 10:55:06 +0100

    Breackthrough Routes cool

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
 import React from 'react';
+import { Route, Switch, Link } from 'react-router-dom';
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 import './App.css';
+
+const HatsPage = (props) =>{
+    console.log(props);
+    return(
+        <div>
+            <h1> HATS PAGE</h1>
+            <Link to='/'>Home</Link>
+            <button onClick={() => props.history.push('/')}>Home</button>
+        </div>
+    );
+};
 
 function App() {
   return(
       <div>
-        <HomePage/>
+          <Switch>
+              <Route exact path='/' component={HomePage}/>
+              <Route path='/hats' component={HatsPage}/>
+          </Switch>
       </div>
   );
 }
